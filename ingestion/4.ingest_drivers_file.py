1fc81d6fe09bb4047c2847f5f7d6ed0d7e32fa0e
tejaswinidhupad <tejaswinidhupad>
2024-12-17 13:29:55 +0000

    drivers file added

--- ingestion/4.ingest_drivers_file.py
+++ ingestion/4.ingest_drivers_file.py
@@ -9,10 +9,73 @@
 
 # COMMAND ----------
 
-from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DateType
 
 # COMMAND ----------
 
-name_schema = StructType(fields=[StructField("forename"),StringType(),True),
-                                 StructField("surname"),StringType(),True)
+name_schema = StructType(fields=[StructField("forename",StringType(),True),
+                                 StructField("surname",StringType(),True)
                                  ])
+
+# COMMAND ----------
+
+drivers_schema = StructType(fields=[StructField("driverId",IntegerType(),False),
+                                    StructField("driverRef",StringType(),True),
+                                    StructField("number",IntegerType(),True),
+                                    StructField("code",StringType(),True),
+                                    StructField("name",name_schema),
+                                    StructField("dob",DateType(),True),
+                                    StructField("nationality",StringType(),True),
+                                    StructField("url",StringType(),True)
+
+])
+
+# COMMAND ----------
+
+drivers_df = spark.read \
+             .schema(drivers_schema) \
+             .json("dbfs:/mnt/formula1projectdl/raw/drivers.json")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 - Rename columns and add new columns
+# MAGIC 1. driverId renamed to driver_id  
+# MAGIC 1. driverRef renamed to driver_ref  
+# MAGIC 1. ingestion date added
+# MAGIC 1. name added with concatenation of forename and surname
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp, col, lit, concat
+
+# COMMAND ----------
+
+driver_with_columns_df = drivers_df.withColumnRenamed("driverId","driver_id") \
+                                   .withColumnRenamed("driverRef","driver_ref") \
+                                   .withColumn("ingestion_date",current_timestamp()) \
+                                   .withColumn("name",concat(col("name.forename"),lit(" "),col("name.surname")))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Drop the unwanted columns
+# MAGIC 1. url
+# MAGIC
+
+# COMMAND ----------
+
+drivers_final_df = driver_with_columns_df.drop(col("url"))
+
+# COMMAND ----------
+
+display(drivers_final_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 4 - Write to output to processed container in parquet format
+
+# COMMAND ----------
+
+drivers_final_df.write.mode("overwrite").parquet("/mnt/formula1projectdl/processed/drivers")
